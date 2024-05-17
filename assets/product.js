d524bd97183bae99ce9b83269f612214ba07730b
Christopher Dodd <christopherrdodd@gmail.com>
2024-05-17 09:20:10 +0800

    Variant selection with two options

--- assets/product.js
+++ assets/product.js
@@ -1,7 +1,24 @@
 document.addEventListener('DOMContentLoaded', function() {
     document.querySelector('.shopify-product-form').addEventListener('input', function(evt) {
         const variants = JSON.parse(document.querySelector('#product-variants-json').innerText);
-        const current_variant_id = document.querySelector('#current-variant').value;
+
+        let current_variant_id = null;
+
+        if(evt.target.id != 'current-variant') {
+            const option1 = this.querySelector('select[name="option1"]')?.value || null;
+            const option2 = this.querySelector('select[name="option2"]')?.value || null;
+            const option3 = this.querySelector('select[name="option3"]')?.value || null;
+
+            const variant_from_options = variants.find(variant => 
+                variant.option1 == option1 &&
+                variant.option2 == option2 &&
+                variant.option3 == option3
+            )
+            
+            current_variant_id = variant_from_options.id;
+            document.querySelector('#current-variant').value = current_variant_id;
+        }
+        
         const current_variant = variants.find(variant => variant.id == current_variant_id);
         console.log(current_variant);
 
