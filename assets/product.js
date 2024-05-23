5726d9660197bd4ceed4109cc0338471d31d3336
Christopher Dodd <christopherrdodd@gmail.com>
2024-05-23 09:26:59 +0800

    Refactor product variant selection to use custom web component

--- assets/product.js
+++ assets/product.js
@@ -1,42 +1,57 @@
-document.addEventListener('DOMContentLoaded', function() {
-    document.querySelector('.shopify-product-form').addEventListener('input', function(evt) {
-        const variants = JSON.parse(document.querySelector('#product-variants-json').innerText);
+class ProductPage extends HTMLElement {
+    constructor() {
+        super();
+    }
 
-        let current_variant_id = null;
-
-        if(evt.target.id != 'current-variant') {
-            const option1 = this.querySelector('select[name="option1"]')?.value || null;
-            const option2 = this.querySelector('select[name="option2"]')?.value || null;
-            const option3 = this.querySelector('select[name="option3"]')?.value || null;
-
-            const variant_from_options = variants.find(variant => 
-                variant.option1 == option1 &&
-                variant.option2 == option2 &&
-                variant.option3 == option3
-            )
-            
-            current_variant_id = variant_from_options.id;
-            document.querySelector('#current-variant').value = current_variant_id;
-        }
+    connectedCallback() {
+        this.formElement = this.querySelector('form');
+        this.handle = this.getAttribute('handle');
+        this.atcButton = this.querySelector('.atc-button');
+        this.variants = JSON.parse(document.querySelector('#product-variants-json').innerText);
+        this.currentVariant = this.variants.find(variant => variant.id == document.querySelector('#current-variant').value);
         
-        const current_variant = variants.find(variant => variant.id == current_variant_id);
-        console.log(current_variant);
+        this.formElement.addEventListener('input', this.updateVariant.bind(this));
+    }
 
-        document.querySelector('.product-price').innerText = Shopify.formatMoney(current_variant.price);
-        document.querySelector('.product-image').src = current_variant.featured_image.src;
-        if(current_variant.featured_image.alt != null) document.querySelector('.product-image').alt = current_variant.featured_image.alt;
+    variantFromOptionValues() {
+        const option1 = this.querySelector('select[name="option1"]')?.value || null;
+        const option2 = this.querySelector('select[name="option2"]')?.value || null;
+        const option3 = this.querySelector('select[name="option3"]')?.value || null;
 
+        const variant_from_options = this.variants.find(variant => 
+            variant.option1 == option1 &&
+            variant.option2 == option2 &&
+            variant.option3 == option3
+        )
+        return variant_from_options;
+    }
 
-        const atcButton = document.querySelector('.atc-button');
-        if(current_variant.available) {
-            atcButton.removeAttribute('disabled');
-            atcButton.innerText = "Add to cart";
+    updateVariant() {
+        this.currentVariant = this.variantFromOptionValues();
+        console.log(this.currentVariant);
+
+        // 1. Update the hidden variant field
+        document.querySelector('#current-variant').value = this.currentVariant.id;
+
+        // 2. Update history state
+        window.history.replaceState({}, '', `/products/${this.handle}?variant=${this.currentVariant.id}`);
+
+        // 3. Update image
+        document.querySelector('.product-image').src = this.currentVariant.featured_image.src;
+        if(this.currentVariant.featured_image.alt != null) document.querySelector('.product-image').alt = this.currentVariant.featured_image.alt;
+
+        // 4. Update price
+        document.querySelector('.product-price').innerText = Shopify.formatMoney(this.currentVariant.price);
+
+        // 5. Update button state
+        if(this.currentVariant.available) {
+            this.atcButton.removeAttribute('disabled');
+            this.atcButton.innerText = "Add to cart";
         } else {
-            atcButton.setAttribute('disabled', '');
-            atcButton.innerText = "SOLD OUT";
+            this.atcButton.setAttribute('disabled', '');
+            this.atcButton.innerText = "SOLD OUT";
         }
+    }
+}
 
-        const product_handle = document.querySelector('#current-variant').getAttribute('data-product-handle');
-        window.history.replaceState({}, '', `/products/${product_handle}?variant=${current_variant_id}`);
-    });
-});
+customElements.define('product-page', ProductPage);
